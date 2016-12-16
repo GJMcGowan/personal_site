9868a8a600007cd6e552f23d8df2714552746e69
GJMcGowan <gemcgow@gmail.com>
2016-12-16 15:38:02 +0000

    Initial Commit - based on 'create-react-app'

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './HomePage.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Welcome!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
