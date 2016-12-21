a911b57aff1b52be63969b64549cd66561a2d9bd
GJMcGowan <gemcgow@gmail.com>
2016-12-21 20:43:52 +0000

    Adds Loader to about page, changes picture location, fixes warnings from webpack

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
-import CentreBox from '../CentreBox/CentreBox';
 import './About.css';
+import me from './me.jpg';
+
+import Loader from '../Loader/Loader';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageLoaded: false
+    };
+  }
+
+  onLoad() {
+    this.setState({imageLoaded: true});
+  }
+
   render() {
     return (
       <div className='col-xs-10 col-xs-offset-1 standard-container'>
-        <img src='/me.jpg' className='img-fluid bio-image' />
-        <div className='bio-text'>
-          <p>I'm a web developer, living in London</p>
+        <div hidden={this.state.imageLoaded}>
+          <Loader />
+        </div>
+        <div hidden={!this.state.imageLoaded}>
+          <img
+            src={me}
+            className='img-fluid bio-image'
+            alt='my picture'
+            onLoad={this.onLoad.bind(this)}/>
+          <div className='bio-text'>
+            <p>I'm a web developer, living in London</p>
+          </div>
         </div>
       </div>
     );
--- src/components/Interests/Interests.js
+++ src/components/Interests/Interests.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import CentreBox from '../CentreBox/CentreBox';
-
 class Interests extends Component {
   render() {
     return (
