50b699528f7fb176402fa7b09f5289aff863bef2
GJMcGowan <gemcgow@gmail.com>
2016-12-21 13:54:43 +0000

    Adds routing and minor other changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HomePage from './components/HomePage/HomePage';
+import { browserHistory } from 'react-router';
 
+import Routes from './routes';
 import './index.css';
 
 ReactDOM.render(
-  <HomePage />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
